261c559d61cfb4425869870e23f1dc39f564a82f
shavkatbek11 <shavkatbekq69@gmail.com>
2023-03-26 11:43:22 +0500

    accordion react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import './App.css'
+import SingleQuestion from './components/Question';
+import Question from './components/Question';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ questions, setQuestions ] = useState(data);
+	const [questionInfo, setQuestionInfo] = useState('');
+
+	function close(id) {
+		if( id === questionInfo) {
+			setQuestionInfo("")
+		}else{
+			setQuestionInfo(id)
+		}
+	}
+
+	return (
+		<main>
+			<div className="container">
+				<h3>questions and answers about login</h3>
+				<section className="info">
+				 {questions.map((item) => {
+					return(
+						<Question 
+						key={item.id}
+						info={questionInfo == item.id ? item.info : ''}
+						title={item.title}
+						id={item.id}
+						close={close}
+						/>
+					)
+				 })}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/Question.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = (props) => {
+	const [ showInfo, setShowInfo ] = React.useState(false);
+	const {title, info, id} = props;
+	const {item} = props;
+	const {close} = props;
+	const onCilickItem=(id)=>{
+		setShowInfo((prevValue)=> !prevValue)
+	}
+
+
+	
+	return (
+		<article className={'question'}>
+			<header>
+				<h4>{title}</h4>
+				<button
+					className="btn"
+					onClick={() => {
+						close(id)
+					}}
+				>
+					{info.length ? '-' : '+'}
+				</button>
+			</header>
+			 <p>{info}</p>
+		</article>
+	);
+};
+
+export default Question;
